170c6c3cc121c63a61e91c97377295d127e09c90
Zak1012-a <156977956+Zak1012-a@users.noreply.github.com>
2024-06-10 10:19:40 -0500

    Update db.js

--- Node Project/db.js
+++ Node Project/db.js
@@ -1,10 +1,12 @@
 //Postgress Connection
 const { Client } = require('pg')
 const client = new Client({
-  host: '',
-  database: '',
-  port: '',
-})
+  host: 'localhost',
+  user:'root',
+  password: 'root',
+  database: 'master',
+  port: '3030',
+});
 client.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
