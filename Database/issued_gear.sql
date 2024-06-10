57a1c595807c6a24c512a600aeefe6a092e1cc2c
Zak1012-a <156977956+Zak1012-a@users.noreply.github.com>
2024-06-10 12:23:26 -0500

    Delete Database/issued_gear.sql

--- Database/issued_gear.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE issued_gear (
-item_issued       varchar(80), --(Item Issued)
-item_type         varchar(80), --(Name of Item Issued)
-item_sn           int,         --(Serial Number of Item)
-cadet_name        varchar(80), --(Last, First)
-date_issued       DATE,        --(YYYY-MM-DD)
-returned          BOOLEAN,     --(TRUE or FALSE) 
-date_returned     DATE,        --(YYYY-MM-DD [0000-00-00 if still being used])
-);
