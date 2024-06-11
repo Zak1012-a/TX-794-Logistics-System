870598b27c6ad3cd74d0b637cd60b405184f238e
Zak1012-a <156977956+Zak1012-a@users.noreply.github.com>
2024-06-11 08:07:52 -0500

    Update user-input.php

--- Project/php/user-input.php
+++ Project/php/user-input.php
@@ -1,3 +1,13 @@
-<?php
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <?php 
+      INSERT INTO cadet_data ("Alpha","Logistics-Officer","Kossbiel, Zakary","c/A1C","229991","TRUE","Full",)
 
-?>
+
+
+    ?>
+  </body>
+</html>
