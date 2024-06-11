685e83baa582fea8ec26b36fd8610ac95e4d0e5d
Zak1012-a <156977956+Zak1012-a@users.noreply.github.com>
2024-06-11 08:41:49 -0500

    Update user-input.php

--- Project/php/user-input.php
+++ Project/php/user-input.php
@@ -3,11 +3,35 @@
     
   </head>
   <body>
-    <?php 
-      INSERT INTO cadet_data ("Alpha","Logistics-Officer","Kossbiel, Zakary","c/A1C","229991","TRUE","Full",)
+    <?php
+      // MySQL Database Login
+      $servername = "localhost";
+      $username = "username";
+      $password = "password";
+      $dbname = "database_name";
 
+      // Create connection
+      $conn = new mysqli($servername, 
+      $username, $password, $dbname);
 
+      // Check connection
+      if ($conn->connect_error) {
+      die("Connection failed: " 
+          . $conn->connect_error);
+      }
 
+      
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      // collect value of input field
+      $data = $_REQUEST['val1'];
+
+      if (empty($data)) {
+          echo "data is empty";
+      } else {
+          echo $data;
+      }
+    }
     ?>
   </body>
 </html>
