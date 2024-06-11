c34d4707a49fe9d3c7575b9c6e25c31a8e68c19e
Zak1012-a <156977956+Zak1012-a@users.noreply.github.com>
2024-06-11 09:00:26 -0500

    Update user-input.php

--- Project/php/user-input.php
+++ Project/php/user-input.php
@@ -1,37 +1,43 @@
+<!DOCTYPE html>
 <html>
   <head>
-    
+    <title>Form</title>
   </head>
+  
   <body>
-    <?php
-      // MySQL Database Login
-      $servername = "localhost";
-      $username = "username";
-      $password = "password";
-      $dbname = "database_name";
+    <center>
+      <?php
+            // servername     =>     localhost
+            // username       =>     root
+            // password       =>     empty
+            // database name  =>     master
+            $conn = mysqli_connect("localhost","root","","staff")
 
-      // Create connection
-      $conn = new mysqli($servername, 
-      $username, $password, $dbname);
+            // Check Connection
+            if($conn === false){
+              die("ERROR: Couldn't Connect. "
+                 . mysqli_connect_error())
+            }
 
-      // Check connection
-      if ($conn->connect_error) {
-      die("Connection failed: " 
-          . $conn->connect_error);
-      }
-
-      
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-      // collect value of input field
-      $data = $_REQUEST['val1'];
-
-      if (empty($data)) {
-          echo "data is empty";
-      } else {
-          echo $data;
-      }
-    }
-    ?>
+            // Taking Data Input from Form
+            $flight = $_REQUEST['flight'];
+            $cadet_role =  $_REQUEST['cadet_role'];
+            $cadet_name_first =  $_REQUEST['cadet_name_first'];
+            $cadet_rank =  $_REQUEST['cadet_rank'];
+            $cadet_id =  $_REQUEST['cadet_id'];
+            $cadet_year =  $_REQUEST['cadet_year'];
+            $passing    =  $_REQUEST['passing'];
+            $hq_paperwork =  $_REQUEST['hq_paperwork'];
+            $awards =  $_REQUEST['awards'];
+            $cords = $_REQUEST['cords'];
+            $community_service_hours = $_REQUEST['community_service_hours'];
+            $drill_team = $_REQUEST['drill_team'];
+            $saber_team = $_REQUEST['saber_team'];
+            $color_guard = $_REQUEST['color_guard'];
+            
+            
+            
+      ?>
+    </center>
   </body>
 </html>
