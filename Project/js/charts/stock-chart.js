7cb184a5cd06bbba0be80a5c962459bbe2084418
Zak1012-a <156977956+Zak1012-a@users.noreply.github.com>
2024-06-13 10:39:48 -0500

    Update stock-chart.js

--- Project/js/charts/stock-chart.js
+++ Project/js/charts/stock-chart.js
@@ -1,1 +1,21 @@
-
+// Bar chart
+new Chart(document.getElementById("stock-chart"), {
+    type: 'bar',
+    data: {
+      labels: ["PLACEHOLDER", "PLACEHOLDER", "PLACEHOLDER", "PLACEHOLDER", "PLACEHOLDER"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'PLACEHOLDER'
+      }
+    }
+});
