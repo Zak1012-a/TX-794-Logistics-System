afc784267a9ced6e5dfa6a4d58acbbdd8b6c0ec2
Zak1012-a <156977956+Zak1012-a@users.noreply.github.com>
2024-06-13 11:21:07 -0500

    Update collective-community-service-chart.js

--- Project/js/charts/collective-community-service-chart.js
+++ Project/js/charts/collective-community-service-chart.js
@@ -1,1 +1,18 @@
-
+<canvas id="pie-chart" width="800" height="450"></canvas>
+new Chart(document.getElementById("pie-chart"), {
+    type: 'pie',
+    data: {
+      labels: ["Africa", "Asia", ],
+      datasets: [{
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [2478,5267,734,784,433]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
